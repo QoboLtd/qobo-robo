84820cb8da64fd18af5968bca292a627986a053e
Alexander Mamchenkov <mamtchenkov@gmail.com>
2018-07-19 16:34:52 +0300

    Added mysql:import and mysql:export commands (task #6638)

---
+++ src/Command/Mysql/Import.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Copyright (c) Qobo Ltd. (https://www.qobo.biz)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Qobo Ltd. (https://www.qobo.biz)
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Qobo\Robo\Command\Mysql;
+
+use Qobo\Robo\AbstractCommand;
+use Qobo\Robo\Formatter\PropertyList;
+
+class Import extends AbstractCommand
+{
+    /**
+     * Import mysql dump
+     *
+     * @param string $db Database name
+     * @param string $file Destination file for a dump
+     * @param string $user MySQL user to bind with
+     * @param string $pass (Optional) MySQL user password
+     * @param string $host (Optional) MySQL server host
+     * @param string $port (Optional) MySQL server port
+     * @option string $format Output format (table, list, csv, json, xml)
+     * @option string $fields Limit output to given fields, comma-separated
+     *
+     * @return PropertyList result
+     */
+    public function mysqlImport($db, $file = 'etc/mysql.sql', $user = 'root', $pass = null, $host = null, $port = null, $opts = ['format' => 'table', 'fields' => ''])
+    {
+        $result = $this->taskMysqlImport()
+            ->db($db)
+            ->file($file)
+            ->user($user)
+            ->pass($pass)
+            ->host($host)
+            ->port($port)
+            ->hide($pass)
+            ->run();
+
+        if (!$result->wasSuccessful()) {
+            $this->exitError("Failed to run command");
+        }
+
+        return true;
+    }
+}
